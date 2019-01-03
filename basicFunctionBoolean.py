11956cdd688bcfa144b5fa4ae88cc01fd6bf9786
insantana <46345426+insantana@users.noreply.github.com>
2019-01-03 11:24:27 -0300

    Create class basicFunctionBoolean.py

---
+++ basicFunctionBoolean.py
@@ -0,0 +1,12 @@
+# Operação booleana - igual
+print(5 == 8)
+# Operação booleana - maior que
+print(5 > 8)
+# Operação booleana - menor que
+print(5 < 8)
+# Operação booleana - diferença
+print(5 != 8)
+# Operação booleana - maior ou igual
+print(5 >= 8)
+# Operação booleana - menor ou igual
+print(5 <= 8)
